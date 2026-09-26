f07bb9e36b7f333d31945a73c46285c4cc77c8b8
workshop4u <workshop4u2026@gmail.com>
2026-09-26 12:42:55 +0530

    Update HelloController.java

--- src/main/java/com/example/working/HelloController.java
+++ src/main/java/com/example/working/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String home() {
         return """
-		<font color=red><h1>Welcome all to CI/CD example
+		<font color=red><h1>Welcome all to CI/CD example changed
 		""";
     }
 }
